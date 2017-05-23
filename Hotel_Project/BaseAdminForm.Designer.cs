aae2ca9063a665b9764227e2d683a6965764b385
floatGOAT <pandarpr@yandex.ru>
2017-05-23 18:57:42 +0600

    Apply renaming

---
+++ Hotel_Project/BaseAdminForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Hotel_Project
+{
+    partial class BaseAdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userpassword = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.username,
+            this.userpassword});
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.RowHeadersWidth = 50;
+            this.dataGridView1.Size = new System.Drawing.Size(510, 261);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // username
+            // 
+            this.username.HeaderText = "Логин";
+            this.username.Name = "username";
+            // 
+            // userpassword
+            // 
+            this.userpassword.HeaderText = "Пароль";
+            this.userpassword.Name = "userpassword";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(127, 70);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(126, 22);
+            this.toolStripMenuItem1.Text = "Добавить";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(126, 22);
+            this.toolStripMenuItem2.Text = "Сменить";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(126, 22);
+            this.toolStripMenuItem3.Text = "Удалить";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // BaseAdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "BaseAdminForm";
+            this.Text = "Окно администратора базы данных";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userpassword;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
---
+++ Hotel_Project/HotelAdminForm.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Hotel_Project
+{
+    public partial class HotelAdminForm : Form
+    {
+        ExcelSerializer excelSerializer = new ExcelSerializer();
+        WordSerializer wordSerializer = new WordSerializer();
+        All_Visitors spisok = new All_Visitors();
+        XmlSerializer serial = new XmlSerializer(typeof(All_Visitors));
+
+        public string fio, number, roomnumber, rangroom, capacityroom, howlong; //for textboxs
+        public string fname; //adress for button
+        public int first = 0; //index for button "next" and "previous"
+        public AllUsers listusers = new AllUsers();
+        public XmlSerializer serial1 = new XmlSerializer(typeof(AllUsers));
+
+        private static string filename = "slaves.xml";
+
+        public HotelAdminForm()
+        {
+            InitializeComponent();
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            fio = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            number = textBox2.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            howlong = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            roomnumber = textBox4.Text;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            capacityroom = radioButton1.Text;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            capacityroom = radioButton2.Text;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            capacityroom = radioButton3.Text;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rangroom = comboBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Visitor currentVisitor = new Visitor(fio, number, roomnumber, Convert.ToUInt16(capacityroom), rangroom, Convert.ToUInt16(howlong));
+            /*
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.Filter = "XML files(*.xml)|*.xml|All files(*.*)|*.*";
+            if (svf.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = svf.FileName;*/
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                spisok = (All_Visitors)serial.Deserialize(fs);
+            }
+            spisok.addvisitor(currentVisitor);
+
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                serial.Serialize(fs, spisok);
+            }
+            using (StreamWriter wlog = new StreamWriter("log.txt", true))
+            {
+                wlog.WriteLine("{0} | {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString() + " | " + "В базу добавлен новый клиент - " + fio);
+            }
+            /*DataSet ds = new DataSet();
+            ds.WriteXml(filename);*/
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "XML files(*.xml)|*.xml|All files(*.*)|*.*";
+            if (opf.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = opf.FileName;*/
+            fname = filename;
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                spisok = (All_Visitors)serial.Deserialize(fs);
+
+                textBox1.Text = spisok.visitors[0].FIO;
+                textBox2.Text = spisok.visitors[0].Phone_Number;
+                textBox4.Text = spisok.visitors[0].Room_Number;
+                comboBox1.Text = spisok.visitors[0].Room_Rang;
+                switch (spisok.visitors[0].Room_Capacity)
+                {
+                    case 1:
+                        radioButton1.Checked = true;
+                        break;
+                    case 2:
+                        radioButton2.Checked = true;
+                        break;
+                    case 4:
+                        radioButton3.Checked = true;
+                        break;
+                }
+                textBox3.Text = Convert.ToString(spisok.visitors[0].Duration);
+
+                if (spisok.visitors.Count > 1)
+                {
+                    button3.Visible = true;
+                }
+                else
+                    button3.Visible = false;
+
+                if (spisok.visitors.Count > 1 && first != 0)
+                {
+                    button4.Visible = true;
+                }
+                else
+                    button4.Visible = false;
+
+            }
+            string filetext = File.ReadAllText(filename);
+            //textBox5.Text = filetext;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = new FileStream(fname, FileMode.OpenOrCreate))
+            {
+                All_Visitors newvisitors = (All_Visitors)serial.Deserialize(fs);
+                int n = newvisitors.visitors.Count;
+                if (first < (n - 1))
+                {
+                    first++;
+                    button3.Visible = true;
+                    button4.Visible = true;
+                }
+                textBox1.Text = newvisitors.visitors[first].FIO;
+                textBox2.Text = newvisitors.visitors[first].Phone_Number;
+                textBox4.Text = newvisitors.visitors[first].Room_Number;
+                comboBox1.Text = newvisitors.visitors[first].Room_Rang;
+                switch (newvisitors.visitors[first].Room_Capacity)
+                {
+                    case 1:
+                        radioButton1.Checked = true;
+                        break;
+                    case 2:
+                        radioButton2.Checked = true;
+                        break;
+                    case 4:
+                        radioButton3.Checked = true;
+                        break;
+                }
+                textBox3.Text = Convert.ToString(newvisitors.visitors[first].Duration);
+                if (first < (n - 1) && first > 0)
+                {
+                    button3.Visible = true;
+                    button4.Visible = true;
+                }
+                else
+                {
+                    button3.Visible = false;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = new FileStream(fname, FileMode.OpenOrCreate))
+            {
+                All_Visitors newvisitors = (All_Visitors)serial.Deserialize(fs);
+                int n = newvisitors.visitors.Count;
+                if (first > 0)
+                {
+                    first--;
+                    button4.Visible = true;
+                    button3.Visible = true;
+                }
+                textBox1.Text = newvisitors.visitors[first].FIO;
+                textBox2.Text = newvisitors.visitors[first].Phone_Number;
+                textBox4.Text = newvisitors.visitors[first].Room_Number;
+                comboBox1.Text = newvisitors.visitors[first].Room_Rang;
+                switch (newvisitors.visitors[first].Room_Capacity)
+                {
+                    case 1:
+                        radioButton1.Checked = true;
+                        break;
+                    case 2:
+                        radioButton2.Checked = true;
+                        break;
+                    case 4:
+                        radioButton3.Checked = true;
+                        break;
+                }
+                textBox3.Text = Convert.ToString(newvisitors.visitors[first].Duration);
+                if (first > 0 && first < (n - 1))
+                {
+                    button3.Visible = true;
+                    button4.Visible = true;
+                }
+                else
+                {
+                    button4.Visible = false;
+                }
+            }
+        }
+
+        private void Excel_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Visitor currentVisitor = new Visitor(fio, number, roomnumber, Convert.ToUInt16(capacityroom), rangroom, Convert.ToUInt16(howlong));
+            excelSerializer.isExisted();
+            excelSerializer.Write(currentVisitor);
+            excelSerializer.Close();
+        }
+
+        private void Word_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Visitor currentVisitor = new Visitor(fio, number, roomnumber, Convert.ToUInt16(capacityroom), rangroom, Convert.ToUInt16(howlong));
+            wordSerializer.isExisted();
+            wordSerializer.Write(currentVisitor);
+            wordSerializer.Close();
+        }
+
+        private void About_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void toolStripExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    [Serializable]
+    public class Visitor
+    {
+        public List<Visitor> visitors;
+
+        public const UInt16 Standard = 1000;
+        public const UInt16 Lux = 5000;
+        public const UInt16 SuperLux = 10000;
+
+        public string FIO, Phone_Number;
+        public string Room_Number;
+        public UInt16 Room_Capacity;
+        public String Room_Rang;
+        public UInt16 Duration;
+
+        public Visitor(string fio, string phone_number, string room_number, UInt16 capacity, String rang, UInt16 duration)
+        {
+            FIO = fio;
+            Phone_Number = phone_number;
+            Room_Number = room_number;
+            Room_Capacity = capacity;
+            Room_Rang = rang;
+            Duration = duration;
+        }
+        public Visitor()
+        {
+
+        }
+        public int Counting_Value()
+        {
+            int a = 0;
+            switch (Room_Rang)
+            {
+                case "Стандартный":
+                    a = Room_Capacity * Standard * Duration;
+                    break;
+                case "Люкс":
+                    a = Room_Capacity * Lux * Duration;
+                    break;
+                case "Суперлюкс":
+                    a = Room_Capacity * SuperLux * Duration;
+                    break;
+            }
+            return a;
+        }
+        public void show()
+        {
+            Console.WriteLine("Гость: " + FIO + "\n" + "Номер телефона: " + Phone_Number + "\n" + "Номер комнаты: " + Room_Number + "\n" + "Класс номера: " + Room_Rang + "\n" + "Количество мест: " + Room_Capacity + "\n" + "Количество дней: " + Duration + "\n" + "Выручка: " + Counting_Value() + "\n");
+        }
+    }
+    [Serializable]
+    public class All_Visitors
+    {
+        public List<Visitor> visitors = new List<Visitor>();
+        public All_Visitors()
+        {
+
+        }
+        public void addvisitor(string fio, string phone_number, string room_number, UInt16 capacity, String rang, UInt16 duration)
+        {
+            Visitor visitor = new Visitor(fio, phone_number, room_number, Convert.ToUInt16(capacity), rang, duration);
+            visitor.Counting_Value();
+            visitor.show();
+            visitors.Add(visitor);
+        }
+        public void addvisitor(Visitor visitor)
+        {
+            visitor.Counting_Value();
+            visitor.show();
+            visitors.Add(visitor);
+        }
+    }
