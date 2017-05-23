447b9534a1dd3ef495f783e0d7a0ae44cd0d4b9d
floatGOAT <pandarpr@yandex.ru>
2017-05-23 18:59:14 +0600

    Global project changes

--- Hotel_Project/Program.cs
+++ Hotel_Project/Program.cs
@@ -16,7 +16,7 @@ namespace Hotel_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StartForm());
+            Application.Run(new HotelAdminForm());
         }
     }
 }
